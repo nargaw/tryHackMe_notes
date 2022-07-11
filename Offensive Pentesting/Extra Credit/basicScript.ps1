7510c04deb9bd7a84874e766d3d7400c1b500464
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2022-07-11 01:34:34 -0700

    Create basicScript.ps1

---
+++ Offensive Pentesting/Extra Credit/basicScript.ps1
@@ -0,0 +1,11 @@
+$system_ports = Get-NetTCPConnection -State Listen
+
+$text_port = Get-Content -Path C:\Users\Administrator\Desktop\ports.txt
+
+foreach($port in $text_port){
+
+    if($port -in $system_ports.LocalPort){
+        echo $port
+     }
+
+}
